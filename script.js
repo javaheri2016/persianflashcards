2f9b12b90e55bc83ff25decdf769dd70e877db39
javaheri2016 <katarzyna.javaheri@gmail.com>
2017-02-11 15:15:09 +0100

    fiszki 1.0

---
+++ script.js
@@ -0,0 +1,384 @@
+
+$(document).ready(function(){
+         $('#face0').click(function(){
+             $('#card0').css({
+                 "-webkit-transform":" rotateY(180deg)",
+                 "-moz-transform":" rotateY(180deg)",
+                 "-o-transform":" rotateY(180deg)",
+                 "transform":" rotateY(180deg)",
+               
+             });
+         });
+         $('#back0').click(function(){
+             $("#card0").css({
+                 "-webkit-transform":" ",
+                 "-moz-transform":" ",
+                 "-o-transform":" ",
+                 "transform":" ",
+             });
+         });
+    
+     });
+
+    $(document).ready(function(){
+      $('#face1').click(function(){
+             $('#card1').css({
+                 "-webkit-transform":" rotateY(180deg)",
+                 "-moz-transform":" rotateY(180deg)",
+                 "-o-transform":" rotateY(180deg)",
+                 "transform":" rotateY(180deg)",
+               
+             });
+         });
+         $('#back1').click(function(){
+             $("#card1").css({
+                 "-webkit-transform":" ",
+                 "-moz-transform":" ",
+                 "-o-transform":" ",
+                 "transform":" ",
+             });
+         });
+
+ });
+
+    $(document).ready(function(){
+      $('#face2').click(function(){
+             $('#card2').css({
+                 "-webkit-transform":" rotateY(180deg)",
+                 "-moz-transform":" rotateY(180deg)",
+                 "-o-transform":" rotateY(180deg)",
+                 "transform":" rotateY(180deg)",
+               
+             });
+         });
+         $('#back2').click(function(){
+             $("#card2").css({
+                 "-webkit-transform":" ",
+                 "-moz-transform":" ",
+                 "-o-transform":" ",
+                 "transform":" ",
+             });
+         });
+     });
+
+$(document).ready(function(){
+      $('#face3').click(function(){
+             $('#card3').css({
+                 "-webkit-transform":" rotateY(180deg)",
+                 "-moz-transform":" rotateY(180deg)",
+                 "-o-transform":" rotateY(180deg)",
+                 "transform":" rotateY(180deg)",
+               
+             });
+         });
+         $('#back3').click(function(){
+             $("#card3").css({
+                 "-webkit-transform":" ",
+                 "-moz-transform":" ",
+                 "-o-transform":" ",
+                 "transform":" ",
+             });
+         });
+    
+    
+});
+
+
+
+$(document).ready(function(){
+      $('#face4').click(function(){
+             $('#card4').css({
+                 "-webkit-transform":" rotateY(180deg)",
+                 "-moz-transform":" rotateY(180deg)",
+                 "-o-transform":" rotateY(180deg)",
+                 "transform":" rotateY(180deg)",
+               
+             });
+         });
+         $('#back4').click(function(){
+             $("#card4").css({
+                 "-webkit-transform":" ",
+                 "-moz-transform":" ",
+                 "-o-transform":" ",
+                 "transform":" ",
+             });
+         });
+    
+    
+});
+
+$(document).ready(function(){
+      $('#face5').click(function(){
+             $('#card5').css({
+                 "-webkit-transform":" rotateY(180deg)",
+                 "-moz-transform":" rotateY(180deg)",
+                 "-o-transform":" rotateY(180deg)",
+                 "transform":" rotateY(180deg)",
+               
+             });
+         });
+         $('#back5').click(function(){
+             $("#card5").css({
+                 "-webkit-transform":" ",
+                 "-moz-transform":" ",
+                 "-o-transform":" ",
+                 "transform":" ",
+             });
+         });
+    
+    
+});
+
+
+$(document).ready(function(){
+      $('#face6').click(function(){
+             $('#card6').css({
+                 "-webkit-transform":" rotateY(180deg)",
+                 "-moz-transform":" rotateY(180deg)",
+                 "-o-transform":" rotateY(180deg)",
+                 "transform":" rotateY(180deg)",
+               
+             });
+         });
+         $('#back6').click(function(){
+             $("#card6").css({
+                 "-webkit-transform":" ",
+                 "-moz-transform":" ",
+                 "-o-transform":" ",
+                 "transform":" ",
+             });
+         });
+    
+    
+});
+
+$(document).ready(function(){
+      $('#face7').click(function(){
+             $('#card7').css({
+                 "-webkit-transform":" rotateY(180deg)",
+                 "-moz-transform":" rotateY(180deg)",
+                 "-o-transform":" rotateY(180deg)",
+                 "transform":" rotateY(180deg)",
+               
+             });
+         });
+         $('#back7').click(function(){
+             $("#card7").css({
+                 "-webkit-transform":" ",
+                 "-moz-transform":" ",
+                 "-o-transform":" ",
+                 "transform":" ",
+             });
+         });
+    
+    
+});
+
+
+$(document).ready(function(){
+      $('#face8').click(function(){
+             $('#card8').css({
+                 "-webkit-transform":" rotateY(180deg)",
+                 "-moz-transform":" rotateY(180deg)",
+                 "-o-transform":" rotateY(180deg)",
+                 "transform":" rotateY(180deg)",
+               
+             });
+         });
+         $('#back8').click(function(){
+             $("#card8").css({
+                 "-webkit-transform":" ",
+                 "-moz-transform":" ",
+                 "-o-transform":" ",
+                 "transform":" ",
+             });
+         });
+    
+    
+});
+
+
+$(document).ready(function(){
+      $('#face9').click(function(){
+             $('#card9').css({
+                 "-webkit-transform":" rotateY(180deg)",
+                 "-moz-transform":" rotateY(180deg)",
+                 "-o-transform":" rotateY(180deg)",
+                 "transform":" rotateY(180deg)",
+               
+             });
+         });
+         $('#back9').click(function(){
+             $("#card9").css({
+                 "-webkit-transform":" ",
+                 "-moz-transform":" ",
+                 "-o-transform":" ",
+                 "transform":" ",
+             });
+         });
+    
+    
+});
+
+
+$(document).ready(function(){
+      $('#face10').click(function(){
+             $('#card10').css({
+                 "-webkit-transform":" rotateY(180deg)",
+                 "-moz-transform":" rotateY(180deg)",
+                 "-o-transform":" rotateY(180deg)",
+                 "transform":" rotateY(180deg)",
+               
+             });
+         });
+         $('#back10').click(function(){
+             $("#card10").css({
+                 "-webkit-transform":" ",
+                 "-moz-transform":" ",
+                 "-o-transform":" ",
+                 "transform":" ",
+             });
+         });
+    
+    
+});
+
+
+$(document).ready(function(){
+      $('#face11').click(function(){
+             $('#card11').css({
+                 "-webkit-transform":" rotateY(180deg)",
+                 "-moz-transform":" rotateY(180deg)",
+                 "-o-transform":" rotateY(180deg)",
+                 "transform":" rotateY(180deg)",
+               
+             });
+         });
+         $('#back11').click(function(){
+             $("#card11").css({
+                 "-webkit-transform":" ",
+                 "-moz-transform":" ",
+                 "-o-transform":" ",
+                 "transform":" ",
+             });
+         });
+    
+    
+});
+
+
+$(document).ready(function(){
+      $('#face12').click(function(){
+             $('#card12').css({
+                 "-webkit-transform":" rotateY(180deg)",
+                 "-moz-transform":" rotateY(180deg)",
+                 "-o-transform":" rotateY(180deg)",
+                 "transform":" rotateY(180deg)",
+               
+             });
+         });
+         $('#back12').click(function(){
+             $("#card12").css({
+                 "-webkit-transform":" ",
+                 "-moz-transform":" ",
+                 "-o-transform":" ",
+                 "transform":" ",
+             });
+         });
+    
+    
+});
+
+$(document).ready(function(){
+      $('#face13').click(function(){
+             $('#card13').css({
+                 "-webkit-transform":" rotateY(180deg)",
+                 "-moz-transform":" rotateY(180deg)",
+                 "-o-transform":" rotateY(180deg)",
+                 "transform":" rotateY(180deg)",
+               
+             });
+         });
+         $('#back13').click(function(){
+             $("#card13").css({
+                 "-webkit-transform":" ",
+                 "-moz-transform":" ",
+                 "-o-transform":" ",
+                 "transform":" ",
+             });
+         });
+    
+    
+});
+
+
+$(document).ready(function(){
+      $('#face14').click(function(){
+             $('#card14').css({
+                 "-webkit-transform":" rotateY(180deg)",
+                 "-moz-transform":" rotateY(180deg)",
+                 "-o-transform":" rotateY(180deg)",
+                 "transform":" rotateY(180deg)",
+               
+             });
+         });
+         $('#back14').click(function(){
+             $("#card14").css({
+                 "-webkit-transform":" ",
+                 "-moz-transform":" ",
+                 "-o-transform":" ",
+                 "transform":" ",
+             });
+         });
+    
+    
+});
+
+
+
+
+$(document).ready(function(){
+      $('#face15').click(function(){
+             $('#card15').css({
+                 "-webkit-transform":" rotateY(180deg)",
+                 "-moz-transform":" rotateY(180deg)",
+                 "-o-transform":" rotateY(180deg)",
+                 "transform":" rotateY(180deg)",
+               
+             });
+         });
+         $('#back15').click(function(){
+             $("#card15").css({
+                 "-webkit-transform":" ",
+                 "-moz-transform":" ",
+                 "-o-transform":" ",
+                 "transform":" ",
+             });
+         });
+    
+    
+});
+
+
+$(document).ready(function(){
+      $('#face16').click(function(){
+             $('#card16').css({
+                 "-webkit-transform":" rotateY(180deg)",
+                 "-moz-transform":" rotateY(180deg)",
+                 "-o-transform":" rotateY(180deg)",
+                 "transform":" rotateY(180deg)",
+               
+             });
+         });
+         $('#back16').click(function(){
+             $("#card16").css({
+                 "-webkit-transform":" ",
+                 "-moz-transform":" ",
+                 "-o-transform":" ",
+                 "transform":" ",
+             });
+         });
+    
+    
+});
+
